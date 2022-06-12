c4cde342287b07794a6b5cf3873523081a926ae9
Chirag <chiraggoyel98@gmail.com>
2022-06-13 01:33:43 +0530

    ui changes

---
+++ client/src/component/Chat/Chat.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import {user} from "../Join/Join"
+import socketIO from "socket.io-client";
+import "./Chat.css";
+import sendLogo from "../../images/send.png";
+
+const ENDPOINT = "http://localhost:4500";
+
+
+
+
+const Chat = () => {
+
+    const socket = socketIO(ENDPOINT, {transports: ['websocket'] });
+
+    useEffect(() => {
+      socket.on('connect', ()=> {
+        
+      })
+
+      socket.emit('joined', {})
+      return () => {
+        
+      }
+    }, [socket])
+    
+
+
+  return (
+    <div className='chatPage'>
+        <div className='chatContainer'>
+            <div className='header'></div>
+            <div className='chatBox'></div>
+            <div className='inputBox'>
+               <input type="text" id="chatInput" />
+               <button className='sendBtn'><img src={sendLogo} alt="Send" /></button>
+            </div>
+        </div>       
+    </div>
+  )
+}
+
+export default Chat
